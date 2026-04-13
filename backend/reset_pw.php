937f558381ab6dea2dae789fd90d4a64bdc67c7a
Mohamed Mahmoud <mohmahmoudd63@gmail.com>
2026-04-13 23:43:57 +0200

    chore: cleanup test and unused files

--- backend/reset_pw.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-$app = require_once __DIR__ . '/bootstrap/app.php';
-$app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-$u = \App\Models\User::find(37);
-$u->password = \Illuminate\Support\Facades\Hash::make('12345678');
-$u->save();
-echo "Done! Phone: {$u->phone}\n";
